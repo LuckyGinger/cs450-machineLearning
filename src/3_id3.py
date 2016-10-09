6b305a1d2fd122e5dc1120f46267ab4b842df8cf
Thom <tallen.thom@gmail.com>
2016-10-08 22:33:26 -0600

    Take that world! Got it to create a tree for the iris dataset.

--- src/3_id3.py
+++ src/3_id3.py
@@ -21,6 +21,7 @@ def calc_entropy(p):
 def calc_info_gain(data, targets, feature):
     gain = 0
     n_data = len(data)
+
     # List the values that feature can take
     values = []
     for datapoint in data:
@@ -28,11 +29,9 @@ def calc_info_gain(data, targets, feature):
             values.append(datapoint[feature])
 
     feature_counts = np.zeros(len(values))
-    # print(len(feature_counts))
     entropy = np.zeros(len(values))
     the_entropy = 0.0
     value_index = 0
-    # print(len(values))
     # Find where those values appear in data[feature] and the corresponding class
     for value in values:
         data_index = 0
@@ -42,8 +41,6 @@ def calc_info_gain(data, targets, feature):
                 # feature_counts[value_index] += 1
                 new_targets.append(targets[data_index])
             data_index += 1
-        # print(feature_counts)
-
 
         # Get the values in new_targets
         class_values = []
@@ -57,93 +54,76 @@ def calc_info_gain(data, targets, feature):
                 if aclass == classValue:
                     class_counts[class_index] += 1
             class_index += 1
-        # print(class_values)
-        # print(class_counts)
-        # print(class_index)
-        temp_count = value_index - 1
+
         for class_index in range(len(class_values)):
-            # temp_count += 1
             entropy[value_index] += calc_entropy(float(class_counts[class_index]) / sum(class_counts))
-
-            # print(entropy)
-            # gain += float(feature_counts[value_index]) / n_data * entropy[value_index]
-
-        the_entropy += (sum(class_counts)/len(data)) * (entropy[value_index])
+        gain += float(feature_counts[value_index])/n_data * entropy[value_index]
         value_index += 1
 
-    # print(entropy)
-    # print(value_index)
+    return gain
 
-    return the_entropy
+def make_tree(data, targets, feature_names):
 
-def make_tree(data, targets):
-
-    # Various initializations suppressed
     n_data = len(data)
     n_features = len(data[0])
 
+    #  Get the target names
+    frequency = [x[1] for x in Counter(targets).most_common()]
 
-    default = Counter(targets).most_common(1)[0][0]
-    # if n_data == 0 or n_features == 0:
-    #     # Have reached an empty branch
-    #     return default
-    # elif targets.count(targets[0]) == n_data:
-    #     # Only 1 class remains
-    #     return targets[0]
-    # else:
-    #     Choose which feature is best
+    # Calculate total entropy
+    total_entropy = 0
+    for i in frequency:
+        total_entropy += calc_entropy(float(i) / n_data)
 
-    # branch = np.zeros(n_features)
-    # print(branch)
-    for feature in range(n_features):
-        print(calc_info_gain(data, targets, feature))
-        # branch[feature] = calc_info_gain(data, targets, feature)
+    default = targets[np.argmax(frequency)]
+    if n_data == 0 or n_features == 0:
+        # Have reached an empty branch
+        return default
+    elif Counter(targets[0]) == n_data:
+        # Only 1 class remains
+        return targets[0]
+    else:
+        # Choose which feature is best
+        gain = np.zeros(n_features)
+        for feature in range(n_features):
+            g = calc_info_gain(data, targets, feature)
+            gain[feature] = total_entropy - g
+        best_feature = np.argmax(gain)
+        tree = {feature_names[best_feature]: {}}
 
-    # print(branch)
+        # Get the unique values
+        values = []
+        for datapoint in data:
+            if datapoint[best_feature] not in values:
+                values.append(datapoint[best_feature])
 
-
-        # print(g)
-
-    # print(total)
-            # gain[feature] = totalEntropy - g
-        # best_feature = np.argmax(gain)
-    #     tree = {feature_names[best_feature]: {}}
-    #
-    #     values = []
-    #     for datapoint in data:
-    #         if datapoint[feature] not in values:
-    #             values.append(datapoint[feature])
-    #
-    #     # Find the possible feature values
-    #     for value in values:
-    #         new_data = []
-    #         new_targets = []
-    #         new_names = ""
-    #         index = 0
-    #
-    #         # Find the datapoints with each feature value
-    #         for datapoint in data:
-    #             if datapoint[best_feature] == value:
-    #                 if best_feature == 0:
-    #                     datapoint = datapoint[1:]
-    #                     new_names = feature_names[1:]
-    #                 elif best_feature == n_features:
-    #                     datapoint = datapoint[:-1]
-    #                     new_names = feature_names[:-1]
-    #                 else:
-    #                     datapoint = datapoint[:best_feature]
-    #                     datapoint.extend(datapoint[best_feature + 1:])
-    #                     new_names = feature_names[:best_feature]
-    #                     new_names.extend(feature_names[best_feature + 1:])
-    #                 new_data.append(datapoint)
-    #                 new_targets.append(targets[index])
-    #             index += 1
-    #         # Now recurse to the next level
-    #         subtree = make_tree(new_data, new_targets, new_names)
-    #         # and on returning, add the subtree on to the tree
-    #         tree[feature_names[best_feature]][value] = subtree
-    #     return tree
-    return
+        # Find the possible feature values
+        for value in values:
+        # Find the datapoints with each feature value
+            new_data = []
+            new_targets = []
+            index = 0
+            for datapoint in data:
+                if datapoint[best_feature] == value:
+                    if best_feature == 0:
+                        datapoint = datapoint[1:]
+                        new_names = feature_names[1:]
+                    elif best_feature == n_features:
+                        datapoint = datapoint[:-1]
+                        new_names = feature_names[:-1]
+                    else:
+                        datapoint = datapoint[:best_feature]
+                        datapoint.extend(datapoint[best_feature + 1:])
+                        new_names = feature_names[:best_feature]
+                        new_names.extend(feature_names[best_feature + 1:])
+                    new_data.append(datapoint)
+                    new_targets.append(targets[index])
+                index += 1
+            # Now recurse to the next level
+            subtree = make_tree(new_data, new_targets, new_names)
+            # and on returning, add the subtree on to the tree
+            tree[feature_names[best_feature]][value] = subtree
+        return tree
 
 def main():
     split = 0.7
@@ -154,7 +134,10 @@ def main():
     X_data, X_target, y_data, y_target = md.split_data(iris, split)
     # print(X_data)
     # print(X_target)
-    make_tree(X_data, X_target)
+    columns = [i for i in range(len(X_data[0]))]
+
+    tree = make_tree(X_data, X_target, columns)
+    print(tree)
 
 if __name__ == '__main__':
     main()
