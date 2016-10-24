7918589eff2a614fc9ca8de973efb79cc52756fc
Thom <tallen.thom@gmail.com>
2016-10-23 23:34:09 -0600

    Able to compute the outputs and get the accuracy for each datasets.

--- src/NuralNets/5_NeuralNets.py
+++ src/NuralNets/5_NeuralNets.py
@@ -8,20 +8,20 @@ from learndata import ManipulateData as md
 from collections import Counter
 
 
-
 class Net:
     def __init__(self, data, target, num_neurons=[1], bias=-1):
         self._num_inputs = len(data[0])  # const value
         self._num_neurons = num_neurons  # const value
         self._num_outputs = len(Counter(target).most_common())
         self.data = data
+        self.targets = target
         self.bias = bias
         self.inputs = []
         self.neurons = []
         self.outputs = []
 
-        print(len(Counter(target).most_common()))
-        print(Counter(target).most_common())
+        # print(len(Counter(target).most_common()))
+        # print(Counter(target).most_common())
 
         self._create_neurons()
         self.calculate_outputs()
@@ -29,12 +29,30 @@ class Net:
         # print(len(self.outputs))
         # print(self.outputs)
 
-        self.display_net()
-
+        # self.display_net()
+        # print(self.outputs)
+        print("Accuracy: " + str(self.accuracy()) + "%")
         # for i in range(self._num_neurons):
         #     print(self.neurons[i])
 
         return
+
+    def accuracy(self):
+        targets = [i[0] for i in Counter(self.targets).most_common()]
+        values = {}
+
+        #  Create dictionary of targets
+        for i, target in enumerate(targets):
+            temp_val = list("0" * self._num_outputs)
+            temp_val[i] = "1"
+            values["".join(temp_val)] = target
+
+        #  Get list of computed targets
+        out = [values[i] for i in self.outputs]
+        #  Get list of matches
+        total = out == self.targets
+        #  Return the accuracy
+        return round(Counter(total)[True]/len(self.targets)*100, 2)
 
     def display_net(self):
         for i, x in enumerate(self.neurons):
@@ -52,64 +70,71 @@ class Net:
                 else:
                     self.neurons[x].append(Neuron(self._num_neurons[x - 1]))
 
-        #  Create Output Neurons/Last layer
+        # Create Output Neurons/Last layer
         self.neurons.append([])
         for i in range(self._num_outputs):
             self.neurons[-1].append(Neuron(self._num_neurons[-1]))
 
         return
 
-
     def calculate_outputs(self):
 
         def sigmoid(x):
             return 1 / (1 + np.math.exp(-x))
 
-        #  Todo: make this function not use four nested loops
-
-        #  Todo: incorporate this into the for loop before
-        temp_data = [np.insert(i, 0, self.bias) for i in self.data]  # add the bias to the beginning of each row
-        #  print(temp_data)
-
         temp_h = 0
-        for data in temp_data:
+        #  Todo: OPTIMIZE
+        for data in self.data:
+            data = np.insert(data, 0, self.bias)
             for index_layer, layer in enumerate(self.neurons):
                 for index_neuron, neuron in enumerate(layer):
                     for index_weight, weight in enumerate(neuron.get_weights()):
                         if index_layer == 0:
                             temp_h += data[index_weight] * weight
                         else:
-                            if index_weight == 0:  #  Add the bias node
+                            if index_weight == 0:  # Add the bias node
                                 temp_h += self.bias * weight
                             else:
-                                # [print("1st", neuron) for neuron in self.neurons[index_layer - 1]]
-                                # [print("2nd", neuron) for neuron in self.neurons[index_layer]]
-
-                                #  Get the each neuron a value from the layer before and multiply it by the weight
+                                #  Get each neuron's "a" value from the layer before and multiply it by the weight
                                 temp_h += self.neurons[index_layer - 1][index_weight - 1].get_a() * weight
                     neuron.set_h(round(temp_h, 2))
                     neuron.set_a(round(sigmoid(temp_h), 2))
                     temp_h = 0
+            highest = -1
+            index = 0
+            # out = np.zeros(len(self.neurons[-1]))
+            out = ""
+            for i, x in enumerate(self.neurons[-1]):
+                x = x.get_h()
+                if x > highest:
+                    highest = x
+                    index = i
+            for i in range(len(self.neurons[-1])):
+                if i == index:
+                    out += "1"
+                else:
+                    out += "0"
+            self.outputs.append(out)
         return
 
 
         # def calculate_outputs(self):
-    #     temp_data = [np.insert(i, 0, self.bias) for i in self.data]  # add the bias to the beginning of each row
-    #     # print(temp_data)
-    #
-    #     for data in temp_data:
-    #         total = np.zeros(self._num_neurons)
-    #         for index_neuron, neuron in enumerate(self.neurons):
-    #             for index_weight, weight in enumerate(neuron.get_weights()):
-    #                 total[index_neuron] += data[index_weight] * weight
-    #
-    #         for i, x in enumerate(total):
-    #             if x > 0:
-    #                 total[i] = 1
-    #             elif x <= 0:
-    #                 total[i] = 0
-    #         self.outputs.append(total)
-    #     return
+        #     temp_data = [np.insert(i, 0, self.bias) for i in self.data]  # add the bias to the beginning of each row
+        #     # print(temp_data)
+        #
+        #     for data in temp_data:
+        #         total = np.zeros(self._num_neurons)
+        #         for index_neuron, neuron in enumerate(self.neurons):
+        #             for index_weight, weight in enumerate(neuron.get_weights()):
+        #                 total[index_neuron] += data[index_weight] * weight
+        #
+        #         for i, x in enumerate(total):
+        #             if x > 0:
+        #                 total[i] = 1
+        #             elif x <= 0:
+        #                 total[i] = 0
+        #         self.outputs.append(total)
+        #     return
 
 
 class Neuron:
@@ -154,12 +179,13 @@ class Neuron:
 def main():
     split = 0.7
     file = pd.read_csv('test_data.csv').values
-    print(file[0])
+    # print(file[0])
     inputs = len(file[0])
     num_neurons = [10, 2]  # array for the number of neurons in each hidden layer
     X_data, X_target, y_data, y_target = md.split_data(file, split)
-    print(X_data)
+    # print(X_data)
     Net(X_data, X_target, num_neurons=num_neurons)
+    print()
 
     print("Irises Data:")
     iris = ld.load_dataset('iris')
@@ -167,13 +193,16 @@ def main():
     # for i in X_data:
     #     print(i)
     Net(X_data, X_target, num_neurons=num_neurons)
+    print()
 
-    #
-    # print("Pima Indians Data:")
-    # pima = ld.load_dataset('pima')
-    # X_data, X_target, y_data, y_target = md.split_data(pima, split)
+
+    print("Pima Indians Data:")
+    pima = ld.load_dataset('pima')
+    X_data, X_target, y_data, y_target = md.split_data(pima, split)
     # for i in X_data:
     #     print(i)
-    # Net(X_data, X_target, num_neurons=num_neurons)
+    Net(X_data, X_target, num_neurons=num_neurons)
+    print()
+
 if __name__ == '__main__':
     main()
